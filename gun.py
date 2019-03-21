27a643e7c2f03b67cedd526db9dc8d0f4b52b93e
zhao-dapeng <zhaodapeng8023@163.com>
2019-03-21 20:19:07 +0800

    'modify log path'

--- gun.py
+++ gun.py
@@ -10,8 +10,8 @@ loglevel = 'debug'
 #debug = False
 bind = '172.26.26.131:8993'
 #bind = '127.0.0.1:5000'
-pidfile = 'log/gunicorn.pid'
-logfile = 'log/debug.log'
+pidfile = '/mnt/logs/soucangflask/pid/gunicorn.pid'
+logfile = '/mnt/logs/soucangflask/log/debug.log'
 
 #启动的进程数
 workers = multiprocessing.cpu_count() * 1 
