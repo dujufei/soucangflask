49ff33ae9791fbc00a12245277088f5fec6bd477
zhaodapeng <zhaodapeng@haxitag.com>
2019-03-25 16:54:04 +0800

    'add modify'

--- soucangflask.py
+++ soucangflask.py
@@ -13,8 +13,8 @@ from urllib import parse
 from handlelog import Logger
 
 # infologger = Logger('/mnt/data/soucangflask/all.log',level='info')
-# errorlogger = Logger('/mnt/logs/soucangflask/log/errorlog.log',level='error')
-errorlogger = Logger('/Users/zdp/test/soucang.log',level='error')
+errorlogger = Logger('/mnt/logs/soucangflask/log/errorlog.log',level='error')
+# errorlogger = Logger('/Users/zdp/test/soucang.log',level='error')
 
 errortime = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
 
@@ -419,6 +419,6 @@ def abstract():
 
 
 if __name__ == '__main__':
-    app.run(threaded=True)
-    # app.run(threaded=True,host='172.26.26.131',port=8993)
+    # app.run(threaded=True)
+    app.run(threaded=True,host='172.26.26.131',port=8993)
     # app.run()
